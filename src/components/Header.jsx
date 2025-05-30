73ca0229f7e77bcdf1a128b81cabcc17ff6ec7dc
John Stockton <johnstocktoniv@gmail.com>
2025-05-29 20:02:11 -0400

    Fix header on small screens

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,19 @@
 import { pages } from "../constants";
 import { FaGithub, FaLinkedinIn } from "react-icons/fa";
 
-const navButtonStyle = "hover:bg-orange-400 rounded px-1";
+const navButtonStyle = "hover:bg-orange-400 rounded p-1";
 
 export const Header = () => (
-	<header className="flex bg-purple-700 text-gray-200 text-5xl p-3">
-		<span className="grow">
+	<header className="flex bg-purple-700 text-gray-200 text-4xl">
+		<span className="flex flex-col sm:flex-row">
 			{pages.map(page => (
 				<a href={page.route} key={page.name} className={navButtonStyle}>
 					{page.name}
 				</a>
 			))}
 		</span>
-		<span className="flex items-center">
+		<span className="grow" />
+		<span className="flex flex-col sm:flex-row justify-center">
 			<a href="https://github.com/johncannothearyou" target="_blank" rel="noreferrer" className={navButtonStyle}>
 				<FaGithub />
 			</a>
